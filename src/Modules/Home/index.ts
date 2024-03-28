2ff4789be6656efcb863bbe0bd21e9ca73a1e567
Erik Sleziona <erik.sleziona@gmail.com>
2024-03-28 10:22:26 +0100

    Building the ground structure

---
+++ src/Modules/Home/index.ts
@@ -0,0 +1,1 @@
+export * from "./pages/SocialLinkPage/SocialLinkPage";
---
+++ src/Modules/Home/pages/SocialLinkPage/SocialLinkPage.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import "./SocialLinkPage.scss";
+const SocialLinkPage: FC = () => {
+  return <div className="page-container">SocialLinkPage</div>;
+};
+export default SocialLinkPage;
